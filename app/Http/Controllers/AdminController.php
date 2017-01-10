5d07ba72f52e14043bcd30ad221dd2893ef99c53
smbo1997 <smbo1997@mail.ru>
2017-01-10 17:24:55 +0400

    smb_commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,94 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class AdminController extends Controller
-{
-  public function loginForm(){
-      return view('admin.login');
-  }
+class AdminController extends Controller {
+
+    private $data = array();
+
+    public function loginForm() {
+        if (session()->has('admindata')) {
+            return redirect('admin_home');
+        }
+        return view('admin.login');
+    }
+
+    public function checkdata(Request $request) {
+        $check = DB::table('admin')
+                ->where('email', $request->email)
+                ->where('password', $request->password)
+                ->first();
+
+        if ($check->logged !== 1) {
+            $dd = DB::table('admin')
+                    ->where('id', $check->id)
+                    ->update(['logged' => 1]);
+
+            session()->put('admindata', $check->id);
+            return redirect('admin_home');
+        } else {
+            return redirect('admin');
+        }
+    }
+
+    public function adminlogout() {
+        if (!(session()->has('admindata'))) {
+            return redirect('admin');
+        }
+        $logout = DB::table('admin')
+                ->where('id', session()->get('admindata'))
+                ->update(['logged' => 0]);
+        if ($logout) {
+            session()->forget('admindata');
+            return redirect('admin');
+        }
+    }
+
+    public function home() {
+        if (!(session()->has('admindata'))) {
+            return redirect('admin');
+        }
+        return view('admin.home');
+    }
+
+    public function users() {
+        if (!(session()->has('admindata'))) {
+            return redirect('admin');
+        }
+        $users = DB::table('users')
+                ->select('*')
+                ->get();
+        $this->data['users'] = $users;
+        return view('admin.users')->with($this->data);
+    }
+
+    public function deleteuser($id) {
+        $deleteuser = DB::table('users')
+                ->where('id', $id)
+                ->delete();
+        if ($deleteuser) {
+            return redirect('admin_home');
+        }
+    }
+
+    public function updateuserdata(Request $request) {
+        $updateuser = DB::table('users')
+                ->where('id', $request->userId)
+                ->update(['name' => $request->username]);
+        if ($updateuser) {
+            return json_encode(array('data' => 1));
+        }
+    }
+
+    public function viewprofile($id) {
+        if (!(session()->has('admindata'))) {
+            return redirect('admin');
+        }
+        $getuser = DB::table('users')
+                ->where('id', $id)
+                ->first();
+        dd($getuser);
+    }
+
 }
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Styles -->
+        <link href="/css/app.css" rel="stylesheet">
+
+        <!-- Scripts -->
+        <script>
+            window.Laravel = <?php
+echo json_encode([
+    'csrfToken' => csrf_token(),
+]);
+?>
+        </script>
+
+
+        <link rel="stylesheet" href="/css/mycss.css">
+        <link rel="stylesheet" href="/css/bootstrap-responsive.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/font-awesome.css">
+        <link rel="stylesheet" href="/css/font-awesome-ie7.css">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/dataTables.bootstrap.css">
+
+        {{--full calendar--}}
+        <link rel='stylesheet' href="/node_modules/fullcalendar/dist/fullcalendar.css">
+        <link rel='stylesheet' href="/node_modules/fullcalendar/dist/fullcalendar.print.css" media='print'>
+        {{--online jquery--}}
+
+
+
+
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+
+                        <!-- Collapsed Hamburger -->
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+
+
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="app-navbar-collapse" style="margin-left: 30%">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="nav navbar-nav">
+                            <li class="active border-left"><a href="/admin_home">General page</a></li>
+                            <li class="active border-left"><a href="/users">users</a></li>
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="nav navbar-nav navbar-right">
+                            <!-- Authentication Links -->
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    Hi Admin <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="/logout">logout</a>
+                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}                                            
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="container">
+                <div class="row">
+                    <h2 class="username">Hi Admin</h2>
+                </div>
+            </div>
+            <!-- Scripts -->
+            <script src="/js/app.js"></script>
+            <script src="/js/bootstrap.js"></script>
+            <script src="/js/jquery.dataTables.min.js"></script>
+            <script src="/js/dataTables.bootstrap.js"></script>
+
+    </body>
+</html>
